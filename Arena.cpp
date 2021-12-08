d5e10882bf1a5d4bc7ddbc63b9883591cdcad4d1
keddelzz <keddelzz@web.de>
2021-12-08 18:29:39 +0100

    Introduce class core::Arena, a simple bump allocator

---
+++ Arena.cpp
@@ -0,0 +1,51 @@
+//
+// Copyright (c) 2021, Jan Sladek <keddelzz@web.de>
+//
+// SPDX-License-Identifier: BSD-2-Clause
+//
+
+#include "Arena.h"
+
+namespace core {
+
+Arena::Arena()
+    : m_blocks(16)
+{
+    m_blocks.add(Arena::empty_block(Size));
+}
+
+Arena::~Arena()
+{
+    for (const auto &block : m_blocks) free(block.memory);
+}
+
+Arena::Block Arena::empty_block(u64 size)
+{
+    Block block {
+        malloc(size),
+        0,
+        size,
+    };
+    return block;
+}
+
+void *Arena::alloc(u64 size)
+{
+    if (size >= Size) {
+        auto block = Arena::empty_block(size);
+        block.size = size;
+
+        m_blocks.add(block);
+        return block.memory;
+    }
+
+    if (size > m_blocks.last().capacity - m_blocks.last().size)
+        m_blocks.add(Arena::empty_block(Size));
+
+    auto &current = m_blocks.last();
+    auto ptr = (u8 *)current.memory + current.size;
+    current.size += size;
+    return ptr;
+}
+
+}  // namespace core
---
+++ Arena.h
@@ -0,0 +1,49 @@
+//
+// Copyright (c) 2021, Jan Sladek <keddelzz@web.de>
+//
+// SPDX-License-Identifier: BSD-2-Clause
+//
+
+#pragma once
+
+#include <cassert>
+#include <utility>  // std::forward
+
+#include <core/List.h>
+#include <core/Types.h>
+
+namespace core {
+
+class Arena
+{
+public:
+    Arena();
+    ~Arena();
+
+    void *alloc(u64);
+
+    template<typename T, typename... Args>
+    T *construct(Args &&...args)
+    {
+        auto ptr = alloc(sizeof(T));
+        assert(ptr and "core::Arena::alloc returned nullptr");
+        return new (ptr) T(std::forward<Args>(args)...);
+    }
+
+private:
+    constexpr static auto Size = 16384;
+
+    struct Block
+    {
+        void *memory { nullptr };
+        u64 size { 0 };
+        u64 capacity { 0 };
+    };
+    static Block empty_block(u64 size);
+
+    List<Block> m_blocks;
+};
+
+}  // namespace core
+
+using core::Arena;
