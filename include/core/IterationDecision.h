a716ea6ba739273c98945fab7511c0fa197b7c78
keddelzz <keddelzz@web.de>
2022-02-26 10:22:57 +0100

    Introduce core::IterationDecision
    A small enumeration for controlling iteration, when using callbacks.

---
+++ include/core/IterationDecision.h
@@ -0,0 +1,19 @@
+//
+// Copyright (c) 2021, Jan Sladek <keddelzz@web.de>
+//
+// SPDX-License-Identifier: BSD-2-Clause
+//
+
+#pragma once
+
+namespace core {
+
+enum class IterationDecision
+{
+    Break,
+    Continue,
+};
+
+}  // namespace core
+
+using core::IterationDecision;
