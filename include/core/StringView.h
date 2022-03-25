81da4c3ef9be1ff5f19ed00c04bef5e3b5acd1f8
keddelzz <keddelzz@web.de>
2022-03-25 09:40:57 +0100

    Introduce core::StringView::at
    ... because `at(i)` looks better than `(*this)[i]`.

--- include/core/StringView.h
+++ include/core/StringView.h
@@ -24,11 +24,18 @@ public:
     [[nodiscard]] Size size() const { return m_size; }
     [[nodiscard]] bool is_empty() const { return size() == 0; }
     [[nodiscard]] bool non_empty() const { return not is_empty(); }
-    [[nodiscard]] char operator[](Index index) const
+    [[nodiscard]] const char &at(Index index) const
     {
         assert(index < size());
         return *(data() + index);
     }
+    [[nodiscard]] char &at(Index index)
+    {
+        assert(index < size());
+        return *(data() + index);
+    }
+    inline const char &operator[](Index index) const { return at(index); }
+    inline char &operator[](Index index) { return at(index); }
 
     bool operator==(const StringView &other) const;
     bool operator!=(const StringView &other) const
